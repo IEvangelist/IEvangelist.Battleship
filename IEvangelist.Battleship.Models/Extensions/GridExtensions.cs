19b319b5ddf593a7552b125e32f6f74b1df3c855
David Pine <david.pine.7@gmail.com>
2017-05-25 21:18:56 -0500

    Added grid extensions

---
+++ IEvangelist.Battleship.Models/Extensions/GridExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace IEvangelist.Battleship.Models.Extensions
+{
+    public static class GridExtensions
+    {
+        public static bool IsValidShot(this Grid grid, Shot shot)
+            => shot.Row <= grid.RowCount &&
+               shot.Column <= grid.ColumnCount;
+    }
+}
